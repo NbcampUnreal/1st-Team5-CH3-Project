c34f43f0b1462d0080521492daf75b1614733940
hongman321 <goldpig041234@gmail.com>
2025-02-26 16:17:19 +0900

    테스트 게임 모드 삭제 및 베이직 게임모드에 캐릭터 설정

--- Source/FPSProject/Private/TestGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "TestGameMode.h"
-#include "FPSCharacter.h"
-#include "FPSPlayerController.h" // PlayerController 클래스를 사용
-
-ATestGameMode::ATestGameMode()
-{
-	DefaultPawnClass = AFPSCharacter::StaticClass();
-	PlayerControllerClass = AFPSPlayerController::StaticClass();
-}
