7c9e27f7035926633fe4380a1d7ae6f50993544f
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-02-26 21:01:51 +0900

    CoinItem 코드 + 에셋 추가

--- Source/FPSProject/Private/BaseItem.cpp
+++ Source/FPSProject/Private/BaseItem.cpp
@@ -11,7 +11,7 @@ ABaseItem::ABaseItem()
 	SetRootComponent(Scene);
 
 	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
-	Collision->SetCollisionProfileName(TEXT("OverlapDynamic"));
+	Collision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
 	Collision->SetupAttachment(Scene);
 
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
